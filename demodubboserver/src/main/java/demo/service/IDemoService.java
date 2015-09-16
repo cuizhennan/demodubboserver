41f36bec302c96d70f1e95f0264b2f5addef5e7e
Admin123 <cuizhennan@6clue.com>
2015-09-16 18:46:12 +0800

    init project

---
+++ demodubboserver/src/main/java/demo/service/IDemoService.java
@@ -0,0 +1,8 @@
+package demo.service;
+
+/**
+ * @author zhennan-cui @date 2015年9月16日
+ */
+public interface IDemoService {
+
+}
---
+++ demodubboserver/src/main/java/demo/service/impl/DemoServiceImpl.java
@@ -0,0 +1,10 @@
+package demo.service.impl;
+
+import demo.service.IDemoService;
+
+/**
+ * @author zhennan-cui @date 2015年9月16日
+ */
+public class DemoServiceImpl implements IDemoService {
+
+}
