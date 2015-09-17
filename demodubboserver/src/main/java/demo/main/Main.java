d7b383ab4361b637f0e5f06476871e865f0e47d2
Admin123 <cuizhennan@6clue.com>
2015-09-17 10:08:51 +0800

    update main

--- demodubboserver/src/main/java/demo/main/Main.java
+++ demodubboserver/src/main/java/demo/main/Main.java
@@ -1,8 +1,15 @@
 package demo.main;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @author zhennan-cui @date 2015年9月16日
  */
+@SpringBootApplication
 public class Main {
 
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
 }
