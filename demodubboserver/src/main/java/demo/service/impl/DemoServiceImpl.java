d2b355351079ad2d66ed9677298d6be7aec1972c
moxiao <mx@moxiaodeMacBook-Pro.local>
2016-01-17 13:47:34 +0800

    fixbugx

--- demodubboserver/src/main/java/demo/service/impl/DemoServiceImpl.java
+++ demodubboserver/src/main/java/demo/service/impl/DemoServiceImpl.java
@@ -2,18 +2,22 @@ package demo.service.impl;
 
 import java.util.Calendar;
 
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
 import demo.service.IDemoService;
 
 /**
  * @author zhennan-cui @date 2015年9月16日
  */
 public class DemoServiceImpl implements IDemoService {
+	private static final Logger logger = LogManager.getLogger(DemoServiceImpl.class);
 
-    @Override
-    public String sayHello(String args) {
-        args = "Hello " + args + " 2:" + Calendar.getInstance().getTime();
-        System.out.println(args);
+	@Override
+	public String sayHello(String args) {
+		args = "Hello provider " + args + " 2:" + Calendar.getInstance().getTime();
+		logger.info(args);
 
-        return args;
-    }
+		return args;
+	}
 }
